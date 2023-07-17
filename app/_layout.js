bb7851d92df3bbf84c15292d9ae9ba8e36f59d32
assane05e <gueyeazou201629@gmaile.com>
2023-07-17 10:00:57 +0000

    first commit

---
+++ app/_layout.js
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    bold: require("../assets/fonts/DMSans-Bold.ttf"),
+    regular: require("../assets/fonts/DMSans-Medium.ttf"),
+    medium: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) return null;
+
+  return <Stack onlayout={onLayoutRootView} />;
+};
+
+export default Layout;
